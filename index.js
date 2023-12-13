812b961f6c6287d4328453a27708ef28673e26d2
Florent Huck <flovntp@gmail.com>
2023-12-13 13:39:03 +0100

    changing database path

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function openConnection() {
     port: process.env.DATABASE_PORT,
     user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_DATABASE
+    database: process.env.DATABASE_PATH
   });
 }
 function createTable(connection) {
