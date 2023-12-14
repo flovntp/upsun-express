38c6b801cfec0c8fdfa3a6248e5ad69002e757e6
Florent Huck <flovntp@gmail.com>
2023-12-14 14:39:15 +0100

    DATABASE_PASSWORD issue

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function openConnection() {
     host: (process.env.DATABASE_HOST || '127.0.0.1'),
     port: (process.env.DATABASE_PORT || '3306'),
     user: (process.env.DATABASE_USERNAME || 'user'),
-    password: (process.env.DATABASE_PASSWORD || 'express'),
+    password: (process.env.DATABASE_PASSWORD),
     database: (process.env.DATABASE_PATH || 'express')
   });
 }
